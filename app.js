a2a4c8b0bb8a85fb85420501054c596ffba8ee22
bale <githong@kakao.com>
2022-01-15 16:42:07 +0900

    use function in object

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-const player = {
-  name: 'bale',
-  points: 10,
-  fat: true,
+const calculator = {
+  add: (firstNumber, secondNumber) => {
+    console.log(firstNumber + secondNumber);
+  },
+  minus: (firstNumber, secondNumber) => {
+    console.log(firstNumber - secondNumber);
+  },
+  divide: (firstNumber, secondNumber) => {
+    console.log(firstNumber / secondNumber);
+  },
+  powerOf: (firstNumber, secondNumber) => {
+    console.log(firstNumber ** secondNumber);
+  },
 };
 
-console.log(player);
-player.lastName = 'kim';
-player.points = 15;
-console.log(player);
+calculator.add(2, 3);
+calculator.minus(2, 3);
+calculator.divide(2, 3);
+calculator.powerOf(2, 3);
