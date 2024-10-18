fbd1454bddb66807c800c6f475ea5e0eaeeea7a0
githong <githong@kakao.com>
2024-10-18 20:44:52 +0900

    what

--- js/greetings.js
+++ js/greetings.js
@@ -1,50 +1,45 @@
-const loginForm = document.querySelector('#login-form');
-const loginInput = document.querySelector('#login-form input');
-const greeting = document.querySelector('#logined-form');
-const HIDDEN_CLASSNAME = 'hidden';
-const LOGINNED_CLASSNAME = 'loginned';
-const USERNAME_KEY = 'userName';
-const LOGIN_AREA = 'login-form';
-const GREETING_AREA = 'greeting';
-const GREETING_MESSAGE = 'How are you?,';
+const loginForm = document.querySelector('#login-form')
+const loginInput = document.querySelector('#login-form input')
+const greeting = document.querySelector('#logined-form')
+const HIDDEN_CLASSNAME = 'hidden'
+const LOGINNED_CLASSNAME = 'loginned'
+const USERNAME_KEY = 'userName'
+const LOGIN_AREA = 'login-form'
+const GREETING_AREA = 'greeting'
+const GREETING_MESSAGE = 'How are you?,'
 
-let userName = localStorage.getItem(USERNAME_KEY);
+let userName = localStorage.getItem(USERNAME_KEY)
 
 const toggleElement = (elementName) => {
   if (elementName === LOGIN_AREA) {
-    loginForm.classList.toggle(HIDDEN_CLASSNAME);
+    loginForm.classList.toggle(HIDDEN_CLASSNAME)
   } else if (elementName === GREETING_AREA) {
     // set greeting message
-    greeting.querySelector(
-      '#greeting'
-    ).innerText = `${GREETING_MESSAGE} ${userName}`;
-    greeting.classList.toggle(HIDDEN_CLASSNAME);
-    greeting.classList.toggle(LOGINNED_CLASSNAME);
+    greeting.classList.toggle(HIDDEN_CLASSNAME)
+    greeting.classList.toggle(LOGINNED_CLASSNAME)
   }
-};
+}
 
 const onLoginSubmit = (event) => {
   // prevent form event let page refresh
-  event.preventDefault();
+  event.preventDefault()
 
   // get userName and save local storage
-  userName = loginInput.value;
-  localStorage.setItem(USERNAME_KEY, userName);
+  userName = loginInput.value
+  localStorage.setItem(USERNAME_KEY, userName)
 
-  toggleElement(LOGIN_AREA);
-  toggleElement(GREETING_AREA);
-};
+  toggleElement(LOGIN_AREA)
+  toggleElement(GREETING_AREA)
+}
 
 if (userName === null) {
   // register submit event listener
-  loginForm.addEventListener('submit', onLoginSubmit);
+  loginForm.addEventListener('submit', onLoginSubmit)
 
-  toggleElement(LOGIN_AREA);
+  toggleElement(LOGIN_AREA)
 } else {
   // set greeting message
-  greeting.querySelector(
-    '#greeting'
-  ).innerText = `${GREETING_MESSAGE}${userName}`;
+  greeting.querySelector('#greeting').innerText = `${GREETING_MESSAGE}${userName}`
   // show greeting
-  toggleElement(GREETING_AREA);
+  toggleElement(GREETING_AREA)
 }
--- js/weather.js
+++ js/weather.js
@@ -1,21 +1,24 @@
-const API_KEY = 'c075d9c4379bbde28d02ee75d3a31218';
+const API_KEY = 'c075d9c4379bbde28d02ee75d3a31218'
 
 navigator.geolocation.getCurrentPosition(
   async (position) => {
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
+    const lat = position.coords.latitude
+    const lon = position.coords.longitude
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    console.log(lat, lon)
 
-    const response = await fetch(url);
-    const data = await response.json();
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
 
-    const weather = document.querySelector('#weather span:first-child');
-    const city = document.querySelector('#weather span:last-child');
-    weather.innerText = data.name;
-    city.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    const response = await fetch(url)
+    const data = await response.json()
+
+    const weather = document.querySelector('#weather span:first-child')
+    const city = document.querySelector('#weather span:last-child')
+    weather.innerText = data.name
+    city.innerText = `${data.weather[0].main} / ${data.main.temp}`
   },
-  () => {
-    alert("Can't find you. No weather for you.");
+  (t) => {
+    console.log(t)
+    alert("Can't find you. No weather for you.")
   }
-);
+)
